7479a36dfca6566750f95197641e64fde971b591
yangkun.li <1136704398@qq.com>
2021-01-11 23:24:46 +0800

    测试

--- cloud2020Te3st/cloud-provider-payment-8002/src/main/java/com/samsung/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.samsung.service;
-
-import com.samsung.entities.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserService {
-    public int create(User user);
-    public User getUserById(@Param("id") Long id);
-}
